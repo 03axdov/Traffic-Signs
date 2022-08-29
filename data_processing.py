fb93070983499e347e0847b630965b621a2bc31b
Axel Dovskog <axel.dovskog@outlook.com>
2022-08-29 23:16:47 +0200

    Changes to layout and begun working on processing test data

--- data_processing.py
+++ data_processing.py
@@ -2,28 +2,31 @@ import os
 import glob
 import random
 import shutil
+import numpy as np
+import pandas as pd
 
-def split_dataset(ds, train_split=0.8, shuffle=True):
+def split_dataset(ds, train_split=0.9, shuffle=True):
     if shuffle:
        random.shuffle(ds)
     
-    train_size = len(ds)
+    train_size = int(len(ds) * train_split)
     
     train_ds = ds[0:train_size]
     val_ds = ds[train_size+1:]
     
     return train_ds, val_ds
 
-def train_validation_split(train_path, new_train_path, new_validation_path, validation_split=0.1):
+def train_validation_split(train_path, new_train_path, new_validation_path):
     folders = os.listdir(train_path)
-    for folder in folders:
+    for t, folder in enumerate(folders):
         full_path = os.path.join(train_path, folder)
-        images_paths = glob.glob(os.path.join(full_path), '*.png')  # All images are PNGs
-        print(images_paths.shape)
+        images_paths = glob.glob(os.path.join(full_path, '*.png'))  # All images are PNGs
 
         x_train, x_val = split_dataset(images_paths)
-        print(x_train.shape)
-        print(x_val.shape)
+
+        print(f"FOLDER: {t}")
+        print(f"x_train: {np.shape(x_train)}")
+        print(f"x_val: {np.shape(x_val)}")
 
         for x in x_train:
             path_to_folder = os.path.join(new_train_path, folder)
@@ -40,3 +43,12 @@ def train_validation_split(train_path, new_train_path, new_validation_path, vali
                 os.makedirs(path_to_folder)
 
             shutil.copy(x, path_to_folder)
+
+
+def process_test(path, csv_path):
+    test = {}
+
+    csv = pd.read_csv(csv_path)
+    for i in range(len(csv['ClassId'])):
+        img_name = csv['Path'][i].replace('Test/', '')
+        label = csv['ClassId'][i]
